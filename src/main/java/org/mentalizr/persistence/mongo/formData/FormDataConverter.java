58e594eefb6317ff16f0dce79fc38d59b39f2c42
Arthur Picht <picht@arthurpicht.de>
2021-09-23 13:44:51 +0200

    FormDataConverter: extension, refactoring, test

--- src/main/java/org/mentalizr/persistence/mongo/formData/FormDataConverter.java
+++ src/main/java/org/mentalizr/persistence/mongo/formData/FormDataConverter.java
@@ -4,10 +4,68 @@ import org.bson.Document;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.mentalizr.serviceObjects.frontend.patient.formData.ExerciseSO;
+import org.mentalizr.serviceObjects.frontend.patient.formData.FeedbackSO;
 import org.mentalizr.serviceObjects.frontend.patient.formData.FormDataSO;
 import org.mentalizr.serviceObjects.frontend.patient.formData.FormElementDataSO;
 
 public class FormDataConverter {
+
+    private static class ExerciseConverter {
+
+        public static Document convert(ExerciseSO exerciseSO) {
+            return new Document(ExerciseSO.SENT, exerciseSO.isSent())
+                    .append(ExerciseSO.LAST_MODIFIED_TIMESTAMP, exerciseSO.getLastModifiedTimestamp())
+                    .append(ExerciseSO.SEEN_BY_THERAPIST, exerciseSO.isSeenByTherapist())
+                    .append(ExerciseSO.SEEN_BY_THERAPIST_TIMESTAMP, exerciseSO.getSeenByTherapistTimestamp());
+        }
+
+        public static ExerciseSO convert(Document document) {
+            boolean sent = document.getBoolean(ExerciseSO.SENT);
+            String lastModifiedTimestamp = document.getString(ExerciseSO.LAST_MODIFIED_TIMESTAMP);
+            boolean seenByTherapist = document.getBoolean(ExerciseSO.SEEN_BY_THERAPIST);
+            String seenByTherapistTimestamp = document.getString(ExerciseSO.SEEN_BY_THERAPIST_TIMESTAMP);
+
+            return new ExerciseSO(sent, lastModifiedTimestamp, seenByTherapist, seenByTherapistTimestamp);
+        }
+    }
+
+    private static class FormElementDataConverter {
+
+        public static Document convert(FormElementDataSO formElementDataSO) {
+            return new Document(FormElementDataSO.FORM_ELEMENT_ID, formElementDataSO.getFormElementId())
+                    .append(FormElementDataSO.FORM_ELEMENT_TYPE, formElementDataSO.getFormElementType())
+                    .append(FormElementDataSO.FORM_ELEMENT_VALUE, formElementDataSO.getFormElementValue());
+        }
+
+        public static FormElementDataSO convert(Document document) {
+            FormElementDataSO formElementDataSO = new FormElementDataSO();
+            formElementDataSO.setFormElementType(document.getString(FormElementDataSO.FORM_ELEMENT_TYPE));
+            formElementDataSO.setFormElementId(document.getString(FormElementDataSO.FORM_ELEMENT_ID));
+            formElementDataSO.setFormElementValue(document.getString(FormElementDataSO.FORM_ELEMENT_VALUE));
+            return formElementDataSO;
+        }
+    }
+
+    private static class FeedbackConverter {
+
+        public static Document convert(FeedbackSO feedbackSO) {
+            return new Document(FeedbackSO.TEXT, feedbackSO.getText())
+                    .append(FeedbackSO.CREATED_TIMESTAMP, feedbackSO.getCreatedTimestamp())
+                    .append(FeedbackSO.SEEN_BY_PATIENT, feedbackSO.isSeenByPatient())
+                    .append(FeedbackSO.SEEN_BY_PATIENT_TIMESTAMP, feedbackSO.getSeenByPatientTimestamp());
+        }
+
+        public static FeedbackSO convert(Document document) {
+            FeedbackSO feedbackSO = new FeedbackSO();
+            feedbackSO.setText(document.getString(FeedbackSO.TEXT));
+            feedbackSO.setCreatedTimestamp(document.getString(FeedbackSO.CREATED_TIMESTAMP));
+            feedbackSO.setSeenByPatient(document.getBoolean(FeedbackSO.SEEN_BY_PATIENT));
+            feedbackSO.setSeenByPatientTimestamp(document.getString(FeedbackSO.SEEN_BY_PATIENT_TIMESTAMP));
+            return feedbackSO;
+        }
+    }
 
     public static Document convert(FormDataSO formDataSO) {
 
@@ -15,18 +73,22 @@ public class FormDataConverter {
                 .append(FormDataSO.CONTENT_ID, formDataSO.getContentId())
                 .append(FormDataSO.EDITABLE, formDataSO.isEditable());
 
-        List<Document> formElementDataList = new ArrayList<>();
-
-        for (FormElementDataSO formElementDataSO : formDataSO.getFormElementDataList()) {
-
-            Document formElementDataDocument = new Document(FormElementDataSO.FORM_ELEMENT_TYPE, formElementDataSO.getFormElementType())
-                    .append(FormElementDataSO.FORM_ELEMENT_ID, formElementDataSO.getFormElementId())
-                    .append(FormElementDataSO.FORM_ELEMENT_VALUE, formElementDataSO.getFormElementValue());
-            formElementDataList.add(formElementDataDocument);
-
+        if (formDataSO.isExercise()) {
+            Document exerciseDocument = ExerciseConverter.convert(formDataSO.getExerciseSO());
+            document.append(FormDataSO.EXERCISE, exerciseDocument);
         }
 
+        List<Document> formElementDataList = new ArrayList<>();
+        for (FormElementDataSO formElementDataSO : formDataSO.getFormElementDataList()) {
+            Document formElementDataDocument = FormElementDataConverter.convert(formElementDataSO);
+            formElementDataList.add(formElementDataDocument);
+        }
         document.append(FormDataSO.FORM_ELEMENT_DATA_LIST, formElementDataList);
+
+        if (formDataSO.hasFeedback()) {
+            Document feedbackDocument = FeedbackConverter.convert(formDataSO.getFeedbackSO());
+            document.append(FormDataSO.FEEDBACK, feedbackDocument);
+        }
 
         return document;
     }
@@ -42,15 +104,18 @@ public class FormDataConverter {
         formDataSO.setContentId(contentId);
         formDataSO.setEditable(editable);
 
+        Document exerciseDocument = document.get(FormDataSO.EXERCISE, Document.class);
+        if (exerciseDocument != null) {
+            ExerciseSO exerciseSO = ExerciseConverter.convert(exerciseDocument);
+            formDataSO.setExerciseSO(exerciseSO);
+        }
+
         List<Document> formElementDataDocumentList = document.getList(FormDataSO.FORM_ELEMENT_DATA_LIST, Document.class);
         List<FormElementDataSO> formElementDataSOList = new ArrayList<>();
         formDataSO.setFormElementDataList(formElementDataSOList);
 
         for (Document formElementDataDocument : formElementDataDocumentList) {
-            FormElementDataSO formElementDataSO = new FormElementDataSO();
-            formElementDataSO.setFormElementType(formElementDataDocument.getString(FormElementDataSO.FORM_ELEMENT_TYPE));
-            formElementDataSO.setFormElementId(formElementDataDocument.getString(FormElementDataSO.FORM_ELEMENT_ID));
-            formElementDataSO.setFormElementValue(formElementDataDocument.getString(FormElementDataSO.FORM_ELEMENT_VALUE));
+            FormElementDataSO formElementDataSO = FormElementDataConverter.convert(formElementDataDocument);
             formElementDataSOList.add(formElementDataSO);
         }
 
--- src/test/java/org/mentalizr/persistence/mongo/formData/FormDataConverterTest.java
+++ src/test/java/org/mentalizr/persistence/mongo/formData/FormDataConverterTest.java
@@ -1,17 +1,142 @@
 package org.mentalizr.persistence.mongo.formData;
 
+import org.bson.Document;
+import org.bson.json.JsonWriterSettings;
 import org.junit.jupiter.api.Test;
+import org.mentalizr.serviceObjects.frontend.patient.formData.ExerciseSO;
+import org.mentalizr.serviceObjects.frontend.patient.formData.FormDataSO;
+import org.mentalizr.serviceObjects.frontend.patient.formData.FormElementDataSO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FormDataConverterTest {
 
-    @Test
-    void convert() {
-        System.out.println("Test!");
+    private static final boolean outputJson = true;
+
+    private FormDataSO createSimpleFormDataSO() {
+        FormDataSO formDataSO = new FormDataSO();
+        formDataSO.setUserId("userId");
+        formDataSO.setContentId("contentId");
+        formDataSO.setEditable(true);
+
+        List<FormElementDataSO> formElementDataSOList = new ArrayList<>();
+        FormElementDataSO formElementDataSO = new FormElementDataSO();
+        formElementDataSO.setFormElementId("elementId");
+        formElementDataSO.setFormElementType("elementType");
+        formElementDataSO.setFormElementValue("elementValue");
+        formElementDataSOList.add(formElementDataSO);
+
+        formDataSO.setFormElementDataList(formElementDataSOList);
+
+        return formDataSO;
+    }
+
+    private void assertSimpleFormDataSO(FormDataSO formDataSO) {
+        assertEquals("userId", formDataSO.getUserId());
+        assertEquals("contentId", formDataSO.getContentId());
+        assertTrue(formDataSO.isEditable());
+        assertNotNull(formDataSO.getFormElementDataList());
+        List<FormElementDataSO> formElementDataSOList = formDataSO.getFormElementDataList();
+        assertEquals(1, formElementDataSOList.size());
+        FormElementDataSO formElementDataSO = formElementDataSOList.get(0);
+        assertEquals("elementId", formElementDataSO.getFormElementId());
+        assertEquals("elementType", formElementDataSO.getFormElementType());
+        assertEquals("elementValue", formElementDataSO.getFormElementValue());
+    }
+
+    private String getSimpleFormDataSOAsJson() {
+        return "{\n" +
+                "  \"userId\": \"userId\",\n" +
+                "  \"contentId\": \"contentId\",\n" +
+                "  \"editable\": true,\n" +
+                "  \"formElementDataList\": [\n" +
+                "    {\n" +
+                "      \"formElementId\": \"elementId\",\n" +
+                "      \"formElementType\": \"elementType\",\n" +
+                "      \"formElementValue\": \"elementValue\"\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+
+    }
+
+    private FormDataSO createExerciseFormDataSO() {
+        FormDataSO formDataSO = createSimpleFormDataSO();
+        ExerciseSO exerciseSO = new ExerciseSO();
+        exerciseSO.setSent(true);
+        exerciseSO.setLastModifiedTimestamp("lastModifiedTimestamp");
+        exerciseSO.setSeenByTherapist(true);
+        exerciseSO.setSeenByTherapistTimestamp("seenByTherapistTimestamp");
+        formDataSO.setExerciseSO(exerciseSO);
+        return formDataSO;
+    }
+
+    private void assertExerciseFormDataSO(FormDataSO formDataSO) {
+        assertSimpleFormDataSO(formDataSO);
+        assertNotNull(formDataSO.getExerciseSO());
+        ExerciseSO exerciseSO = formDataSO.getExerciseSO();
+        assertTrue(exerciseSO.isSent());
+        assertEquals("lastModifiedTimestamp", exerciseSO.getLastModifiedTimestamp());
+        assertTrue(exerciseSO.isSeenByTherapist());
+        assertEquals("seenByTherapistTimestamp", exerciseSO.getSeenByTherapistTimestamp());
+    }
+
+    private String getExerciseFormDataSOAsJson() {
+        return "{\n" +
+                "  \"userId\": \"userId\",\n" +
+                "  \"contentId\": \"contentId\",\n" +
+                "  \"editable\": true,\n" +
+                "  \"exercise\": {\n" +
+                "    \"sent\": true,\n" +
+                "    \"lastModifiedTimestamp\": \"lastModifiedTimestamp\",\n" +
+                "    \"seenByTherapist\": true,\n" +
+                "    \"seenByTherapistTimestamp\": \"seenByTherapistTimestamp\"\n" +
+                "  },\n" +
+                "  \"formElementDataList\": [\n" +
+                "    {\n" +
+                "      \"formElementId\": \"elementId\",\n" +
+                "      \"formElementType\": \"elementType\",\n" +
+                "      \"formElementValue\": \"elementValue\"\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+    }
+
+    private String getPrettyJson(Document document) {
+        JsonWriterSettings.Builder settingsBuilder = JsonWriterSettings.builder().indent(true);
+        return document.toJson(settingsBuilder.build());
     }
 
     @Test
-    void testConvert() {
+    void simple() {
+        FormDataSO formDataSO = createSimpleFormDataSO();
+
+        Document document = FormDataConverter.convert(formDataSO);
+        String json = getPrettyJson(document);
+        if (outputJson) System.out.println(json);
+        String expectedJson = getSimpleFormDataSOAsJson();
+        assertEquals(expectedJson, json);
+
+        formDataSO = FormDataConverter.convert(document);
+        assertSimpleFormDataSO(formDataSO);
     }
+
+    @Test
+    void exercise() {
+
+        FormDataSO formDataSO = createExerciseFormDataSO();
+
+        Document document = FormDataConverter.convert(formDataSO);
+        String json = getPrettyJson(document);
+        if (outputJson) System.out.println(json);
+        String expectedJson = getExerciseFormDataSOAsJson();
+        assertEquals(expectedJson, json);
+
+        formDataSO = FormDataConverter.convert(document);
+        assertExerciseFormDataSO(formDataSO);
+    }
+
 }
