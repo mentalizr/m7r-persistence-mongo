fabbef0af3a2c075d9741c20d688ae0dfffee75d
Arthur Picht <picht@arthurpicht.de>
2022-04-11 07:30:14 +0200

    timestamps from ap/utils-core

--- src/main/java/org/mentalizr/persistence/mongo/formData/FormDataTimestampUpdater.java
+++ src/main/java/org/mentalizr/persistence/mongo/formData/FormDataTimestampUpdater.java
@@ -1,5 +1,6 @@
 package org.mentalizr.persistence.mongo.formData;
 
+import de.arthurpicht.utils.core.strings.Timestamps;
 import org.mentalizr.commons.Dates;
 import org.mentalizr.serviceObjects.frontend.patient.formData.FormDataSO;
 import org.mentalizr.serviceObjects.frontend.patient.formData.FormDataSOs;
@@ -11,7 +12,7 @@ public class FormDataTimestampUpdater {
     public static void markExerciseAsSeenByTherapist(FormDataSO formDataSO) {
         if (FormDataSOs.isExercise(formDataSO) && !formDataSO.getExercise().isSeenByTherapist()) {
             formDataSO.getExercise().setSeenByTherapist(true);
-            formDataSO.getExercise().setSeenByTherapistTimestamp(Dates.currentTimestampAsISO());
+            formDataSO.getExercise().setSeenByTherapistTimestamp(Timestamps.currentAsISO());
             FormDataDAO.createOrUpdate(formDataSO);
         }
     }
@@ -23,7 +24,7 @@ public class FormDataTimestampUpdater {
     public static void markFeedbackAsSeenByPatient(FormDataSO formDataSO) {
         if (FormDataSOs.hasFeedback(formDataSO) && !formDataSO.getFeedback().isSeenByPatient()) {
             formDataSO.getFeedback().setSeenByPatient(true);
-            formDataSO.getFeedback().setSeenByPatientTimestamp(Dates.currentTimestampAsISO());
+            formDataSO.getFeedback().setSeenByPatientTimestamp(Timestamps.currentAsISO());
             FormDataDAO.createOrUpdate(formDataSO);
         }
     }
