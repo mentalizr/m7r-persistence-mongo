83c66006d0e1bb6bdaf90e282a71c081288a01c7
Fabian Kalk <fabian.kalk@comrize.de>
2024-03-11 18:14:14 +0100

    [BE-4] message overhaul, use of Request objects and CLI command overhaul (WIP)

--- src/main/java/org/mentalizr/persistence/mongo/activityStatus/ActivityStatusMessageConverter.java
+++ src/main/java/org/mentalizr/persistence/mongo/activityStatus/ActivityStatusMessageConverter.java
@@ -1,12 +1,17 @@
 package org.mentalizr.persistence.mongo.activityStatus;
 
 import org.bson.Document;
-import org.mentalizr.serviceObjects.frontend.application.ActivityStatusMessageSO;
+import org.mentalizr.serviceObjects.userManagement.ActivityStatusMessageCollectionSO;
+import org.mentalizr.serviceObjects.userManagement.ActivityStatusMessageSO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ActivityStatusMessageConverter {
 
     public static Document convert(ActivityStatusMessageSO activityStatusMessageSO) {
-        Document document = new Document(ActivityStatusMessageSO.TIMESTAMP, activityStatusMessageSO.getTimestamp());
+        Document document = new Document();
+        document.append(ActivityStatusMessageSO.TIMESTAMP, activityStatusMessageSO.getTimestamp());
         document.append(ActivityStatusMessageSO.USER_ID, activityStatusMessageSO.getUserId());
         document.append(ActivityStatusMessageSO.REST_ID, activityStatusMessageSO.getRestId());
         document.append(ActivityStatusMessageSO.ROLE, activityStatusMessageSO.getRole());
@@ -17,12 +22,34 @@ public class ActivityStatusMessageConverter {
 
     public static ActivityStatusMessageSO convert(Document document) {
         ActivityStatusMessageSO activityStatusMessageSO = new ActivityStatusMessageSO();
-        activityStatusMessageSO.setTimestamp(document.getString(ActivityStatusMessageSO.TIMESTAMP));
+        activityStatusMessageSO.setTimestamp(document.getLong(ActivityStatusMessageSO.TIMESTAMP));
         activityStatusMessageSO.setUserId(document.getString(ActivityStatusMessageSO.USER_ID));
         activityStatusMessageSO.setRestId(document.getString(ActivityStatusMessageSO.REST_ID));
         activityStatusMessageSO.setRole(document.getString(ActivityStatusMessageSO.ROLE));
         activityStatusMessageSO.setMessage(document.getString(ActivityStatusMessageSO.MESSAGE));
 
         return activityStatusMessageSO;
+    }
+
+    public static List<Document> convertActivityList(List<ActivityStatusMessageSO> activityStatusMessageSOList) {
+        List<Document> documentList = new ArrayList<>();
+
+        activityStatusMessageSOList.forEach(activityStatusMessageSO -> {
+            Document document = new Document();
+            document.append(ActivityStatusMessageSO.TIMESTAMP, activityStatusMessageSO.getTimestamp());
+            document.append(ActivityStatusMessageSO.USER_ID, activityStatusMessageSO.getUserId());
+            document.append(ActivityStatusMessageSO.REST_ID, activityStatusMessageSO.getRestId());
+            document.append(ActivityStatusMessageSO.ROLE, activityStatusMessageSO.getRole());
+            document.append(ActivityStatusMessageSO.MESSAGE, activityStatusMessageSO.getMessage());
+        });
+
+        return documentList;
+    }
+
+    public static ActivityStatusMessageCollectionSO convertDocumentListToCollection(List<Document> documentList) {
+        ActivityStatusMessageCollectionSO messageCollectionSO = new ActivityStatusMessageCollectionSO();
+        documentList.forEach(document -> messageCollectionSO.getCollection().add(convert(document)));
+
+        return messageCollectionSO;
     }
 }
--- src/main/java/org/mentalizr/persistence/mongo/activityStatus/ActivityStatusMessageMongoHandler.java
+++ src/main/java/org/mentalizr/persistence/mongo/activityStatus/ActivityStatusMessageMongoHandler.java
@@ -2,14 +2,17 @@ package org.mentalizr.persistence.mongo.activityStatus;
 
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Sorts;
 import org.bson.Document;
-import org.mentalizr.persistence.mongo.DocumentNotFoundException;
+import org.bson.conversions.Bson;
 import org.mentalizr.persistence.mongo.M7RMongoCollection;
 import org.mentalizr.persistence.mongo.PersistenceMongoContext;
-import org.mentalizr.serviceObjects.frontend.application.ActivityStatusMessageSO;
+import org.mentalizr.serviceObjects.userManagement.ActivityStatusMessageSO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -28,31 +31,77 @@ public class ActivityStatusMessageMongoHandler {
                .collect(Collectors.toList());
     }
 
-    public static List<Document> fetchAllOfUserID(String userid) throws DocumentNotFoundException {
-        Document queryDocument = new Document(ActivityStatusMessageSO.USER_ID, userid);
+    public static List<Document> fetchAllOfUserID(String userId) {
+        Document queryDocument = new Document(ActivityStatusMessageSO.USER_ID, userId);
         FindIterable<Document> iterable = mongoCollection.find(queryDocument);
+
         if (iterable.first() == null) {
-            throw new DocumentNotFoundException("No MessageSO found for userId: " + userid);
+            return new ArrayList<>();
         }
         return StreamSupport
                 .stream(iterable.spliterator(), false)
-                .collect(Collectors.toList());
+                .toList();
     }
 
-    public static List<Document> fetchAllOfServiceID(String serviceId) throws DocumentNotFoundException {
-        Document queryDocument = new Document(ActivityStatusMessageSO.REST_ID, serviceId);
-        FindIterable<Document> iterable = mongoCollection.find(queryDocument);
+    public static List<Document> fetchAllOfUserIDFrom(String userId, Long timestamp) {
+        Bson filter = Filters.and(Filters.gte(ActivityStatusMessageSO.TIMESTAMP, timestamp),
+                Filters.eq(ActivityStatusMessageSO.USER_ID, userId));
+
+        FindIterable<Document> iterable = mongoCollection.find()
+                .filter(filter)
+                .sort(Sorts.ascending(ActivityStatusMessageSO.TIMESTAMP));
         if (iterable.first() == null) {
-            throw new DocumentNotFoundException("No MessageSO found for userId: " + serviceId);
+            return new ArrayList<>();
         }
         return StreamSupport
                 .stream(iterable.spliterator(), false)
-                .collect(Collectors.toList());
+                .toList();
+    }
+
+    public static List<Document> fetchAllOfUserIDUntil(String userId, Long timestamp) {
+        Bson filter = Filters.and(Filters.lte(ActivityStatusMessageSO.TIMESTAMP, timestamp),
+                Filters.eq(ActivityStatusMessageSO.USER_ID, userId));
+
+        FindIterable<Document> iterable = mongoCollection.find()
+                .filter(filter)
+                .sort(Sorts.ascending(ActivityStatusMessageSO.TIMESTAMP));
+        if (iterable.first() == null) {
+            return new ArrayList<>();
+        }
+        return StreamSupport
+                .stream(iterable.spliterator(), false)
+                .toList();
+    }
+
+    public static List<Document> fetchAllOfUserIDBetween(String userId, Long fromTimestamp, Long untilTimestamp) {
+        Bson filter = Filters.and(Filters.eq(ActivityStatusMessageSO.USER_ID, userId),
+                Filters.gte(ActivityStatusMessageSO.TIMESTAMP, fromTimestamp),
+                Filters.lte(ActivityStatusMessageSO.TIMESTAMP, untilTimestamp));
+
+        FindIterable<Document> iterable = mongoCollection.find()
+                .filter(filter)
+                .sort(Sorts.ascending(ActivityStatusMessageSO.TIMESTAMP));
+        if (iterable.first() == null) {
+            return new ArrayList<>();
+        }
+        return StreamSupport
+                .stream(iterable.spliterator(), false)
+                .toList();
     }
 
     public static void insertOne(Document document) {
         try {
             mongoCollection.insertOne(document);
+        } catch (RuntimeException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    public static void deleteOne(ActivityStatusMessageSO activityStatusMessageSO) {
+        Bson filter = Filters.and(Filters.eq(ActivityStatusMessageSO.USER_ID, activityStatusMessageSO.getUserId()),
+                Filters.eq(ActivityStatusMessageSO.TIMESTAMP, activityStatusMessageSO.getTimestamp()));
+        try {
+            mongoCollection.deleteOne(filter);
         } catch (RuntimeException e) {
             logger.error(e.getMessage(), e);
         }
