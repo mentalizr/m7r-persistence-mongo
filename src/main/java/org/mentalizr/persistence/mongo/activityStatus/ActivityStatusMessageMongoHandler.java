62e9e41c3277fb322fae36d8a34ce9108b7dd602
Arthur Picht <picht@arthurpicht.de>
2024-05-14 11:42:37 +0200

    code review

--- src/main/java/org/mentalizr/persistence/mongo/activityStatus/ActivityStatusMessageMongoHandler.java
+++ src/main/java/org/mentalizr/persistence/mongo/activityStatus/ActivityStatusMessageMongoHandler.java
@@ -66,7 +66,6 @@ public class ActivityStatusMessageMongoHandler {
     }
 
     public static long count() {
-        // TODO prüfen ob mongoCollection überhaupt besteht
         return mongoCollection.countDocuments();
     }
 }
